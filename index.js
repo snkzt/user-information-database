793740be4c9bc7fd911ff508232aea34bc1a766b
snkzt <snkzt@mercari.com>
2021-04-11 22:19:39 +0900

    indent modification for .gitignore and index.js, and command change fot Procfile

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'assets')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'assets/html/home.html')))
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }...`))
