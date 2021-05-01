a922b9c999f0d66807acbebf0c09db676daa2133
snkzt <snkzt@mercari.com>
2021-05-01 18:42:13 +0900

    changes for 1: deleted commented out code for index.js and sign, home.html, 2: Removed create table sql command from route in index.js, 3: adding new page home.html, css, js(still empty)

--- index.js
+++ index.js
@@ -12,44 +12,43 @@ app.use(express.static(path.join(__dirname, 'client')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'client/html/home.html')))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }...`))
 
+db.run(`CREATE TABLE IF NOT EXISTS user (
+  user_id INTEGER PRIMARY KEY,
+  user_name STRING NOT NULL,
+  password STRING NOT NULL)`);
 
-  // Create a table just for once
-  app.post('/html/sign.html', function (req, res) {
-    db.run(`CREATE TABLE IF NOT EXISTS user (
-      user_id INTEGER PRIMARY KEY,
-      user_name STRING NOT NULL,
-      password STRING NOT NULL)`);
-
-    // Create a record
-    if (req.body.submit === "Register") {
-      if(req.body.reg_password !== req.body.confirm_password) {
-        console.log("password doesnt match");
-      } else {
-        db.run(`SELECT user_id, user_name FROM user WHERE user_name='${req.body.user_name}'`, (err, rows) => {
-          if (!err) {
-            db.run("INSERT INTO user (user_name, password ) VALUES (?, ?)", req.body.reg_name, req.body.reg_password);
-            console.log(message = "1 new record added");
-            res.sendFile(path.join(__dirname,'client/html/main.html'));
-          } else {
-            console.log(err+"User name already taken");
-          }
-        });
-      }
-    }
-
-    // Search a record
-    if (req.body.submit === "Welcome Back") {
-      db.get(`SELECT user_name, password FROM user WHERE user_name='${req.body.user_name}' AND password='${req.body.password}'`, (err, row) => {
+// Create a table just for once
+ app.post('/html/sign.html', function (req, res) {
+  // Create a record
+  if (req.body.submit === "Register") {
+    if(req.body.reg_password !== req.body.confirm_password) {
+      console.log("password doesnt match");
+    } else {
+      db.run(`SELECT user_id, user_name FROM user WHERE user_name='${req.body.user_name}'`, (err, rows) => {
         if (!err) {
-          if (!row) {
-            console.log("No record matches");
-            res.sendFile(path.join(__dirname,'client/html/sign.html'));
-          } else {
-            res.sendFile(path.join(__dirname,'client/html/main.html'));
-          }
+          db.run("INSERT INTO user (user_name, password ) VALUES (?, ?)", req.body.reg_name, req.body.reg_password);
+          console.log(message = "1 new record added");
+          res.sendFile(path.join(__dirname,'client/html/main.html'));
         } else {
-          console.log(err);
+          console.log(err+"User name already taken");
         }
       });
     }
-  });
+  }
+
+  // Search a record
+  if (req.body.submit === "Welcome Back") {
+    db.get(`SELECT user_name, password FROM user WHERE user_name='${req.body.user_name}' AND password='${req.body.password}'`, (err, row) => {
+      if (!err) {
+        if (!row) {
+          console.log("No record matches");
+          res.sendFile(path.join(__dirname,'client/html/sign.html'));
+        } else {
+          res.sendFile(path.join(__dirname,'client/html/main.html'));
+        }
+      } else {
+        console.log(err);
+      }
+    });
+  }
+});
