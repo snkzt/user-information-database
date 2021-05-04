4c020cd27daf3b5fad4338dc1a62223b930b91ac
snkzt <snkzt@mercari.com>
2021-05-04 19:58:36 +0900

    signin/up logic

--- client/js/sign.js
+++ client/js/sign.js
@@ -1,7 +1,6 @@
 const inForm = document.getElementById('signin-frm');
 const upForm = document.getElementById('signup-frm');
 
-
 // POST data and get response to use the result for user check
 async function postData(url = '', data = {}) {
   const response = await fetch(url, {
@@ -14,18 +13,24 @@ async function postData(url = '', data = {}) {
   });
   const status = response.status;
   const res = await response.json();
-  return { status: status, response: res }; 
+  return { status: status, response: res }
 }
 
 // Check user existence
 inForm.addEventListener('submit', (frm) => {
   frm.preventDefault();
   document.querySelector('.alert').style.display = 'none';
-  
-  postData('/h', inForm)
-    .then((data) => {
+
+  const usrData = {
+    user_name: frm.target.elements[0].value,
+    password: frm.target.elements[1].value
+  }
+  console.log('a')
+  postData('/h', usrData)
+    .then(data => {
       if (data.status === 205) {
         document.querySelector('.user-not-exists').style.display = 'block'
+        inForm.reset();
         return window.location.assign('../html/sign.html')
       } else {
         return window.location.assign('../html/main.html')
@@ -39,24 +44,27 @@ upForm.addEventListener('submit', (frm) => {
   document.querySelector('.alert').style.display = 'none';
 
   const regData = {
-    reg_name: frm.target.element[0].value,
-    reg_password: frm.target.element[0].value
+    reg_name: frm.target.elements[0].value,
+    reg_password: frm.target.elements[1].value
   }
 
-  if (document.getElementById('confirm_password') !== reg_password) {
+  if (document.getElementById('confirm_password').value !== regData.reg_password) {
     document.querySelector('.wrong-password').style.display = 'block';
   } else {
     postData('/h', regData)
       .then((data) => {
-        if (data.staus === 201) {
+        if (data.status === 201) {
           document.querySelector('.sign-up-success').style.display = 'block'
           return window.location.assign('../html/main.html')
-        } else {
+        } else if (data.staus !== 201) {
           document.querySelector('.user-already-exists').style.display = 'block'
         }
       })
   }
 });
+
+inForm.reset();
+upForm.reset();
 
 document.addEventListener('click', (alertPop) => {
 	document.querySelectorAll('.alert').forEach((alert) => {
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
+const app = express()
 const httpError = require('http-status-codes')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const app = express()
 const bodyParser = require('body-parser')
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./server/db/app.db');
@@ -19,35 +19,59 @@ db.run(`CREATE TABLE IF NOT EXISTS user (
   user_name STRING NOT NULL UNIQUE,
   password STRING NOT NULL)`);
 
+
  app.post('/h', function (req, res) {
-  // Create a record
-  if (!req.body.reg_name) {
-    db.run(`SELECT user_id, user_name FROM user WHERE user_name='${req.body.reg_name}'`, (err, rows) => {
+  // Search a record
+  console.log('here')
+  console.log(req.body)
+  console.log(req.body.user_name)
+  if (req.body.user_name) {
+    console.log('here5')
+    db.get(`SELECT user_name, password FROM user WHERE user_name='${req.body.user_name}' AND password='${req.body.password}'`, (err, row) => {
+      console.log('here5.5')
       if (!err) {
-        db.run("INSERT INTO user (user_name, password ) VALUES (?, ?)", req.body.reg_name, req.body.reg_password);
-        console.log(message = "1 new record added");
-        res.status(201);
+        console.log('here6')
+        if (!row) {
+          console.log('here7')
+          console.log("No record matches");
+          res.status(205).send({ exsisting: false });
+        } else {
+          console.log('here8')
+          res.status(200).send({ exsisting: true });
+        }
       } else {
-        console.log(err+"User name already taken");
-        res.status(401);
+        console.log('here9')
+        console.log(err);
+        res.sendStatus(205).send({ exsisting: err });
       }
     });
+    console.log('here5.6')
   }
+  console.log('here10')
 
-  // Search a record
-  if (req.body.submit === "Welcome Back") {
-    db.get(`SELECT user_name, password FROM user WHERE user_name='${req.body.user_name}' AND password='${req.body.password}'`, (err, row) => {
+  // Create a record
+  console.log('here1')
+  console.log(req.body.reg_name)
+  if (req.body.reg_name) {
+    console.log('here2')
+    db.run(`SELECT user_id, user_name FROM user WHERE user_name='${req.body.reg_name}'`, (err, rows) => {
       if (!err) {
-        if (!row) {
-          console.log("No record matches");
-          res.status(205)
+        if (rows) {
+          console.log('here4')
+          console.log(err+"User name already taken");
+          res.status(401).send({ exsisting: false, created: false });
         } else {
-          res.status(200);
+          console.log('here3')
+          console.log(req.body.reg_name, req.body.reg_password)
+          db.run("INSERT INTO user (user_name, password ) VALUES (?, ?)", req.body.reg_name, req.body.reg_password);
+          console.log(message = "1 new record added");
+          res.status(201).send({ exsisting: false, created: true });
         }
       } else {
         console.log(err);
-        res.status(205)
+        res.sendStatus(205).send({ exsisting: err });
       }
     });
   }
+  console.log('here11')
 });
