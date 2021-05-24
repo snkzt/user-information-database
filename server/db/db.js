c328218fd96b35b468f71d392216cfe55f237186
snkzt <snkzt@mercari.com>
2021-05-25 00:07:11 +0900

    Refactored and created service layer

--- server/db/db.js
+++ server/db/db.js
@@ -1,93 +1,92 @@
 const { open } = require('sqlite');
 const sqlite3 = require('sqlite3').verbose();
+
 let db;
 
- (async () => {
+(async () => {
   // open the database
-    db = await open({
+  db = await open({
     filename: './server/db/app.db',
-    driver: sqlite3.Database
-  })
+    driver: sqlite3.Database,
+  });
 
   // Create tables just for once
-    db.run(`CREATE TABLE IF NOT EXISTS user (
+  db.run(`CREATE TABLE IF NOT EXISTS user (
       user_id INTEGER PRIMARY KEY UNIQUE,
-      user_name STRING NOT NULL UNIQUE,
-      password STRING NOT NULL)`
-    );
-    db.run(`CREATE TABLE IF NOT EXISTS list (
+      user_name TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL)`);
+  db.run(`CREATE TABLE IF NOT EXISTS list (
       item_id INTEGER PRIMARY KEY UNIQUE,
       user_id INTEGER NOT NULL,
-      create_date STRING NOT NULL,
-      due_date STRING,
-      item STRING NOT NULL,
+      create_date TEXT NOT NULL,
+      due_date TEXT,
+      item TEXT NOT NULL,
       FOREIGN KEY (user_id) 
       REFERENCES user (user_id)
       ON UPDATE CASCADE
-      ON DELETE CASCADE)`
-    );
-})()
+      ON DELETE CASCADE)`);
+})();
 
-async function checkIn (name) {
+async function checkIn(name) {
   try {
     const result = await db.get(`SELECT user_id, user_name, password FROM user WHERE user_name='${name}'`);
-    return result
+    return result;
   } catch (err) {
-     return(err);
+    return (err);
   }
 }
 
-async function checkUp (name) {
+async function checkUp(name) {
   try {
     const result = await db.get(`SELECT user_id, user_name FROM user WHERE user_name='${name}'`);
-    return result
+    return result;
   } catch (err) {
-     return(err);
+    return (err);
   }
-};
+}
 
-async function createUser (name, hashedPassword) {
+async function createUser(name, hashedPassword) {
   try {
     await db.run(`INSERT INTO user (user_name, password) VALUES ('${name}', '${hashedPassword}')`);
     const result = await db.get(`SELECT user_id, user_name FROM user WHERE user_name='${name}'`);
-    return result
+    return result;
   } catch (err) {
-     return(err);
+    return (err);
   }
-};
+}
 
-async function getList (usrId) {
+async function getList(usrId) {
   try {
     const result = await db.all(`SELECT item_id, create_date, due_date, item FROM list WHERE user_id='${usrId}'`);
-    return result
+    return result;
   } catch (err) {
-     return(err);
+    return (err);
   }
-};
+}
 
-async function createList (usrId, cDate, dDate, item) {
+async function createList(usrId, cDate, dDate, item) {
   try {
     await db.run(`INSERT INTO list (user_id, create_date, due_date, item) VALUES ('${usrId}', '${cDate}', '${dDate}', '${item}')`);
   } catch (err) {
-     return(err);
+    return (err);
   }
-};
+}
 
-async function updateList (itemId, usrId, dDate, item) {
+async function updateList(itemId, usrId, dDate, item) {
   try {
     await db.run(`UPDATE list SET due_date='${dDate}', item='${item}' WHERE user_id='${usrId}' AND item_id='${itemId}'`);
   } catch (err) {
-     return(err);
+    return (err);
   }
-};
+}
 
-async function deleteList (itemId, usrId) {
+async function deleteList(itemId, usrId) {
   try {
     await db.run(`DELETE FROM list WHERE user_id='${usrId}' AND item_id='${itemId}'`);
   } catch (err) {
-     return(err);
+    return (err);
   }
-};
+}
 
 module.exports = {
   checkIn,
@@ -96,5 +95,5 @@ module.exports = {
   getList,
   createList,
   updateList,
-  deleteList
-}
+  deleteList,
+};
--- server/routes/service.js
+++ server/routes/service.js
@@ -1,3 +1,153 @@
-const transport = require('../routes/transport');
-const db = require('../db/db'); 
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+const db = require('../db/db');
 
+
+// Check if the user is already authenticated
+function checkAuthStatus(authCookie) {
+  jwt.verify(authCookie, process.env.ACCESS_TOKEN_SECRET, (err) => {
+    if (err) {
+      console.log(err);
+      return '403';
+    }
+  });
+};
+
+// Check user existense
+async function signInDbQuery(name, pw) {
+  let checkedReturn;
+  const data = await db.checkIn(name);
+  if (data) {
+    const id = data.user_id;
+    const uName = data.user_name;
+    const err = data;
+    if (id && uName) {
+      await bcrypt.compare(pw, data.password)
+        .then(async (result) => {
+          const authenticatedUser = { id, name: uName };
+          if (result) {
+            checkedReturn = setToken(authenticatedUser);
+          } else {
+            checkedReturn = '401'
+          }
+        });
+    } else {
+      console.error(err);
+    }
+  } else {
+    checkedReturn = '404'
+  }
+  return checkedReturn;
+};
+
+// Create a new user
+async function signUpDbQuery(name, pw) {
+  let createdReturn;
+  const hashedPassword = await bcrypt.hash(pw, saltRounds);
+  const data = await db.checkUp(name);
+  if (data) {
+    const uName = data.user_name;
+    const err = data;
+    if (uName) {
+      createdReturn = '401'
+    } else {
+      console.error(err);
+    }
+  } else {
+    const dataC = await db.createUser(name, hashedPassword);
+    console.log('1 new user added');
+    const authenticatedUser = { id: dataC.user_id, name: dataC.user_name };
+    if (dataC.user_id) {
+      createdReturn = setToken(authenticatedUser);
+    } else {
+      createdReturn = '500'
+    }
+  }
+  return createdReturn;
+}
+
+function setToken(authenticatedUser) {
+  const accessToken = jwt.sign(authenticatedUser, process.env.ACCESS_TOKEN_SECRET);
+  return accessToken;
+}
+
+// Middlewear for sending cookie information to client side
+function tokenCheck(authCookie) {
+  let checkedToken;
+  if (authCookie === null) {
+    checkedToken = '401';
+  } else {
+    jwt.verify(authCookie, process.env.ACCESS_TOKEN_SECRET, (err, user) => { 
+      if(err) {
+        checkedToken = '403';
+      } else {
+        checkedToken = user;
+      }
+    });
+  }
+  return checkedToken;
+}
+
+// Get user's lists on the sign in
+async function queryList(usrId) {
+  let queryResult
+  const lists = await db.getList(usrId);
+  if (lists) {
+    queryResult = lists;
+  } else {
+    queryResult = '500';
+  }
+  return queryResult;
+}
+
+// Save new list
+async function newList(usrId, cDate, dDate, item) {
+  let createdList;
+  await db.createList(usrId, cDate, dDate, item);
+  const addedList = await db.getList(usrId);
+  if(addedList) {
+    createdList = addedList;
+  } else {
+    createdList = '500';
+  }
+  return createdList;
+}
+
+// Save updated list
+async function updatedList(itemId, usrId, dDate, item) {
+  let modifiedList;
+  await db.updateList(itemId, usrId, dDate, item);
+  const updatedList = await db.getList(usrId);
+  if(updatedList) {
+    modifiedList = updatedList;
+  } else {
+    modifiedList = '500'
+  }
+  return modifiedList;
+}
+
+// Delete list
+async function deletedList(itemId, usrId){
+  let deletedList;
+  await db.deleteList(itemId, usrId);
+  const changedList = await db.getList(usrId);
+  if (changedList) {
+    deletedList = changedList;
+  } else {
+    deletedList = '500'
+  }
+  return deletedList;
+}
+
+module.exports = {
+  checkAuthStatus,
+  signInDbQuery,
+  signUpDbQuery,
+  tokenCheck,
+  queryList,
+  newList,
+  updatedList,
+  deletedList
+}
