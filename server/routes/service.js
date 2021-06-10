e634d0eb89f0bbbc75f782da0d08aeee47579c8e
snkzt <snkzt@mercari.com>
2021-06-10 19:22:55 +0900

    commit for local change

--- server/routes/service.js
+++ server/routes/service.js
@@ -1,3 +1,133 @@
-const transport = require('../routes/transport');
-const db = require('../db/db'); 
+require('dotenv').config();
+const bcrypt = require('bcrypt');
 
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+const db = require('../db/db');
+
+// Check if the user is already authenticated
+function checkAuthStatus(authCookie) {
+  const authenticated = jwt.verify(authCookie, process.env.ACCESS_TOKEN_SECRET, (err) => {
+    if (err) {
+      console.error(err);
+      return '403';
+    }
+    return '200';
+  });
+  return authenticated;
+}
+
+function setToken(authenticatedUser) {
+  return jwt.sign(authenticatedUser, process.env.ACCESS_TOKEN_SECRET);
+}
+
+// Check user existense
+async function signInDbQuery(name, pw) {
+  const data = await db.checkIn(name);
+  if (data) {
+    const id = data.user_id;
+    const uName = data.user_name;
+    if (id && uName) {
+      const result = await bcrypt.compare(pw, data.password);
+      const authenticatedUser = { id, name: uName };
+      if (result) {
+        return setToken(authenticatedUser);
+      }
+      return '401';
+    }
+    return '404';
+  }
+  return '404';
+}
+
+// Create a new user
+async function signUpDbQuery(name, pw) {
+  const hashedPassword = await bcrypt.hash(pw, saltRounds);
+  const data = await db.checkUp(name);
+  if (data) {
+    const uName = data.user_name;
+    if (uName) {
+      return '401';
+    }
+    return '500';
+  }
+  const dataC = await db.createUser(name, hashedPassword);
+  console.log('1 new user added');
+  const authenticatedUser = { id: dataC.user_id, name: dataC.user_name };
+  if (dataC.user_id) {
+    return setToken(authenticatedUser);
+  }
+  return '500';
+}
+
+// Middleware for sending cookie information to client side
+function tokenCheck(authCookie) {
+  if (authCookie === null) {
+    return '401';
+  }
+  const token = jwt.verify(authCookie, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      console.error(err);
+      return '403';
+    }
+    return user;
+  });
+  return token;
+}
+
+// Get user's item lists on the sign in
+async function queryList(usrId) {
+  const lists = await db.getList(usrId);
+  if (lists) {
+    return lists;
+  }
+  return '500';
+}
+
+// Save new list
+async function newList(usrId, cDate, dDate, item) {
+  const listAdded = await db.createList(usrId, cDate, dDate, item);
+  if (listAdded === '200') {
+    const addedList = await db.getList(usrId);
+    if (addedList) {
+      return addedList;
+    }
+  }
+  return '500';
+}
+
+// Save modified list
+async function updatedList(itemId, usrId, dDate, item) {
+  const listUpdated = await db.updateList(itemId, usrId, dDate, item);
+  if (listUpdated === '200') {
+    const updatedLists = await db.getList(usrId);
+    if (updatedLists) {
+      return updatedLists;
+    }
+  }
+  return '500';
+}
+
+// Delete list
+async function deletedList(itemId, usrId) {
+  const listdeleted = await db.deleteList(itemId, usrId);
+  if (listdeleted === '200') {
+    const changedList = await db.getList(usrId);
+    if (changedList) {
+      return changedList;
+    }
+  }
+  return '500';
+}
+
+module.exports = {
+  checkAuthStatus,
+  setToken,
+  signInDbQuery,
+  signUpDbQuery,
+  tokenCheck,
+  queryList,
+  newList,
+  updatedList,
+  deletedList,
+};
--- server/routes/transport.js
+++ server/routes/transport.js
@@ -1,71 +1,36 @@
-require('dotenv').config();
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const jwt = require('jsonwebtoken');
 const cookie = require('cookie');
-const db = require('../db/db'); 
+const service = require('./service');
 
 // Check if the user is already authenticated
-const routeAccessibility = async (req, res, next) => {
+const routeAccessibility = (req, res, next) => {
   const authCookie = req.cookies.AccessToken;
   if (authCookie) {
-    jwt.verify(authCookie, process.env.ACCESS_TOKEN_SECRET, (err) => {
-      if (err) {
-        console.log(err);
-        return res.sendStatus(403);
-      }
+    const returnCheckAuth = service.checkAuthStatus(authCookie);
+    if (returnCheckAuth === '403') {
+      res.sendStatus(403);
+    } else if (returnCheckAuth === '200') {
       res.redirect('/authenticated');
-    });
+    }
   } else {
     next();
   }
 };
-
-// // Check if the user is already authenticated
-// const routeAccessibility = async (req, res, next) => {
-//   const authCookie = req.cookies.AccessToken;
-//   if (authCookie) {
-//     jwt.verify(authCookie, process.env.ACCESS_TOKEN_SECRET, (err) => {
-//       if (err) {
-//         console.log(err);
-//         return res.sendStatus(403);
-//       }
-//       res.redirect('/authenticated');
-//     });
-//   } else {
-//     next();
-//   }
-// };
 
 // Check user existense
 const signIn = async (req, res) => {
   if (req.body.user_name) {
     const name = req.body.user_name;
     const pw = req.body.password;
-    const data = await db.checkIn(name);
-    if (data) {
-      const id = data.user_id;
-      const uName = data.user_name;
-      const { err } = data;
-      if (id && uName) {
-        await bcrypt.compare(pw, data.password).then(async (result) => {
-          const authenticatedUser = { id, name: uName };
-          if (result) {
-            const accessToken = await setToken(authenticatedUser);
-            res.setHeader('Set-Cookie', cookie.serialize('AccessToken', accessToken, { httpOnly: true }));
-            res.status(200).send({ existing: true });
-          } else {
-            console.log('Incorrect password');
-            res.status(401).send({ existing: true, password: false });
-          }
-        });
-      } else {
-        console.error(err);
-        res.sendStatus(err.code).send(`${err.name}: ${err.message}`);
-      }
-    } else {
+    const token = await service.signInDbQuery(name, pw);
+    if (token === '401') {
+      console.log('Incorrect password');
+      res.status(401).send({ existing: true, password: false });
+    } else if (token === '404') {
       console.log('No record matches');
       res.status(401).send({ existing: false });
+    } else if (token) {
+      res.setHeader('Set-Cookie', cookie.serialize('AccessToken', token, { httpOnly: true }));
+      res.status(200).send({ existing: true });
     }
   } else {
     console.error('User name input empty');
@@ -77,31 +42,17 @@ const signIn = async (req, res) => {
 const signUp = async (req, res) => {
   if (req.body.reg_name) {
     const name = req.body.reg_name;
-    const hashedPassword = await bcrypt.hash(req.body.reg_password, saltRounds);
-    const data = await db.checkUp(name);
-
-    if (data) {
-      const uName = data.user_name;
-      const { err } = data;
-      if (uName) {
-        console.log('User name already taken');
-        res.status(401).send({ existing: false, created: false });
-      } else {
-        console.error(err);
-        res.sendStatus(err.code).send(`${err.name}: ${err.message}`);
-      }
-    } else {
-      const dataC = await db.createUser(name, hashedPassword);
-      console.log('1 new user added');
-      const authenticatedUser = { id: dataC.user_id, name: dataC.user_name };
-      if (dataC.user_id) {
-        const accessToken = await setToken(authenticatedUser);
-        res.setHeader('Set-Cookie', cookie.serialize('AccessToken', accessToken, { httpOnly: true }));
-        res.status(201).send({ existing: false, created: true });
-      } else {
-        console.log('Register failed');
-        res.status(500).send({ existing: false });
-      }
+    const pw = req.body.reg_password;
+    const createdData = await service.signUpDbQuery(name, pw);
+    if (createdData === '401') {
+      console.log('User name already taken');
+      res.status(401).send({ existing: false, created: false });
+    } else if (createdData === '500') {
+      console.log('Register failed');
+      res.status(500).send({ existing: false });
+    } else if (createdData) {
+      res.setHeader('Set-Cookie', cookie.serialize('AccessToken', createdData, { httpOnly: true }));
+      res.status(201).send({ existing: false, created: true });
     }
   } else {
     console.error('User name input empty');
@@ -116,34 +67,30 @@ const signOut = async (req, res, next) => {
   next();
 };
 
-async function setToken(authenticatedUser) {
-  const accessToken = jwt.sign(authenticatedUser, process.env.ACCESS_TOKEN_SECRET);
-  return accessToken;
-}
-
 // Middlewear for sending cookie information to client side
 function authenticateToken(req, res, next) {
   const authCookie = req.cookies.AccessToken;
-  if (authCookie === null) return res.sendStatus(401);
-
-  jwt.verify(authCookie, process.env.ACCESS_TOKEN_SECRET, (err, user) => { // user is a value serialised wich is authenticatedUser
-    if (err) return res.sendStatus(403);
-    req.user = user;
+  const checkedToken = service.tokenCheck(authCookie);
+  if (checkedToken === '401') {
+    res.sendStatus(401);
+  } else if (checkedToken === '403') {
+    res.sendStatus(403);
+  } else if (checkedToken) {
+    req.user = checkedToken;
     next();
-  });
+  }
 }
 
 // Get user's lists on the sign in
 const getList = async (req, res) => {
   const usrId = req.body.id;
-  const lists = await db.getList(usrId);
-
-  if (lists) {
-    res.lists = lists;
-    res.status(200).send(res.lists);
-  } else {
+  const lists = await service.queryList(usrId);
+  if (lists === '500') {
     console.log('Could not retrieve lists');
     res.status(500).send({ existing: false });
+  } else {
+    res.lists = lists;
+    res.status(200).send(res.lists);
   }
 };
 
@@ -153,16 +100,14 @@ const createList = async (req, res) => {
   const cDate = req.body.create_date;
   const dDate = req.body.due_date;
   const { item } = req.body;
-  await db.createList(usrId, cDate, dDate, item);
-  const newList = await db.getList(usrId);
-
-  if (newList) {
-    console.log('1 new list added');
-    res.list = newList;
-    res.status(200).send(res.list);
-  } else {
+  const createdList = await service.newList(usrId, cDate, dDate, item);
+  if (createdList === '500') {
     console.log('Save failed');
     res.status(500).send({ existing: false });
+  } else if (createdList) {
+    console.log('1 new list added');
+    res.list = createdList;
+    res.status(200).send(res.list);
   }
 };
 
@@ -172,38 +117,33 @@ const updateList = async (req, res) => {
   const usrId = req.body.user_id;
   const dDate = req.body.due_date;
   const { item } = req.body;
-  await db.updateList(itemId, usrId, dDate, item);
-  const updatedList = await db.getList(usrId);
-
-  if (updatedList) {
-    res.lists = updatedList;
-    res.status(200).send(res.lists);
-  } else {
+  const updatedList = service.updatedList(itemId, usrId, dDate, item);
+  if (updatedList === '500') {
     console.log('Could not retrieve lists');
     res.status(500).send({ existing: false });
+  } else if (updatedList) {
+    res.lists = updatedList;
+    res.status(200).send(res.lists);
   }
 };
 
-// Save updated list
+// Delete list
 const deleteList = async (req, res) => {
   const itemId = req.body.item_id;
   const usrId = req.body.user_id;
-  await db.deleteList(itemId, usrId);
-  const deletedList = await db.getList(usrId);
-
-  if (deletedList) {
+  const deletedList = service.deletedList(itemId, usrId);
+  if (deletedList === '500') {
+    console.log('Could not retrieve lists');
+    res.status(500).send({ existing: false });
+  } else if (deletedList) {
     console.log('1 list deleted');
     res.lists = deletedList;
     res.status(200).send(res.lists);
-  } else {
-    console.log('Could not retrieve lists');
-    res.status(500).send({ existing: false });
   }
 };
 
 module.exports = {
   routeAccessibility,
-  setToken,
   signIn,
   signUp,
   signOut,
@@ -211,5 +151,5 @@ module.exports = {
   getList,
   createList,
   updateList,
-  deleteList
+  deleteList,
 };
